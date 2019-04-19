e8c422a9f696ca2fcb290bc141cdd9555eb07772
Richard <6692223@qq.com>
2019-04-19 09:52:49 +0000

    管理后台 - 商品列表 -5.2

---
+++ app/Admin/Controllers/ProductsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class ProductsController extends Controller
+{
+    use HasResourceActions;
+
+    public function index(Content $content)
+    {
+       return Admin::content(function (Content $content) {
+          $content->header('商品列表');
+          $content->body($this->grid());
+       });
+    }
+
+
+    protected function grid()
+    {
+        return Admin::grid(Product::class, function (Grid $grid) {
+            $grid->id('ID')->sortable();
+            $grid->title('商品名称');
+            $grid->on_sale('已上架')->display(function ($value) {
+                return $value ? '是' : '否';
+            });
+            $grid->price('价格');
+            $grid->rating('评分');
+            $grid->sold_count('销量');
+            $grid->review_count('评论数');
+
+            $grid->actions(function ($actions) {
+                $actions->disableDelete();
+            });
+            $grid->tools(function ($tools) {
+                // 禁止批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+        });
+    }
+
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->id('Id');
+        $show->title('Title');
+        $show->description('Description');
+        $show->image('Image');
+        $show->on_sale('On sale');
+        $show->rating('Rating');
+        $show->sold_count('Sold count');
+        $show->review_count('Review count');
+        $show->price('Price');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    protected function form()
+    {
+        $form = new Form(new Product);
+
+        $form->text('title', 'Title');
+        $form->textarea('description', 'Description');
+        $form->image('image', 'Image');
+        $form->switch('on_sale', 'On sale')->default(1);
+        $form->decimal('rating', 'Rating')->default(5.00);
+        $form->number('sold_count', 'Sold count');
+        $form->number('review_count', 'Review count');
+        $form->decimal('price', 'Price');
+
+        return $form;
+    }
+}
